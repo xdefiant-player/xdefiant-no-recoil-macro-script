e7f42925fa87b258a6a12b042572d219169a1450
xdefiant-player <tacoelga@gmail.com>
2024-08-21 05:30:31 +0500

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,11 @@
+# step 1
+step_1 = goto("discord link (discord.gg/5MS29Mxczw)")
+
+# step 2
+step_2 = watch("join the discord")
+
+# step 3
+step_3 = full_information("for further information about this check our YT channel https://www.youtube.com/@little_edge")
+
+# step 4
+step_4 = get_the_software("Join Discord and message in #get-recoil-software channel.")
